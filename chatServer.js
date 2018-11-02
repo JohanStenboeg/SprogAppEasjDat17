04fd0810f6dc99d5a023385d117191e6417fdbc0
mikk4211 <mikk4211@edu.easj.dk>
2018-11-02 10:04:18 +0100

    Testet - tilføjet kommentarer.

--- chatServer.js
+++ chatServer.js
@@ -7,10 +7,14 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+// Hvilken port, serveren lytter til (den port du skal indtaste, efter hostnavn)
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
 
+/* Definerer al tekst, der vedrører usernames, når en bruger er connected,
+således at det, som brugeren indtaster i tekstfielded bliver sat til username.
+*/
 io.on('connection', function(socket){
     socket.on('new_client',function(username){
     socket.broadcast.emit('new_client', socket.username);
