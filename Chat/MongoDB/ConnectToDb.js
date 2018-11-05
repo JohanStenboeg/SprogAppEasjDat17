433412d2308454aaa1ef058c5f360dae640f236a
wexr <wexr.kk@gmail.com>
2018-11-05 09:28:22 +0100

    forced commit

--- Chat/MongoDB/ConnectToDb.js
+++
@@ -1,8 +0,0 @@
-var MongoClient = require('mongodb').MongoClient;
-
-var uri = "mongodb+srv://Stenboeg:Stenboeg@cluster0.mongodb.net/SprogAppTest";
-MongoClient.connect(uri, function(err, client) {
-   const collection = client.db("SprogAppTest").collection("Brugere");
-   // perform actions on the collection object
-   client.close();
-});
