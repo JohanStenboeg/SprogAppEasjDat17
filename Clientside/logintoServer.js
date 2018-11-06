ae41eccec1a456e45c15b5e56df77948f39c8d2f
Michael Hansen <michaelhansenit@live.dk>
2018-11-06 09:19:58 +0100

    Opdaterede links på de forskellige html-sider

--- Clientside/logintoServer.js
+++ Clientside/logintoServer.js
@@ -27,22 +27,22 @@ function testLogin(){
                 switch(this.response){
                     case "Kursist":
                     //opretCookie("brugernavn", brugernavninput.value);
-                    window.location.href= "http://127.0.0.1:5500/menu/kursist/kursistside.html"
+                    window.location.href= "http://127.0.0.1:5500/login/menu/kursist/kursistside.html"
                     //opretCookie("brugernavn", brugernavninput.value);
                     break;
 
                     case "Sprogmakker":
-                    window.location.href= "http://127.0.0.1:5500/menu/sprogmakker/sprogmakkerside.html"
+                    window.location.href= "http://127.0.0.1:5500/login/menu/sprogmakker/sprogmakkerside.html"
                     document.cookie="loginPrivilege="+this.response+""
                     break;
 
                     case "Underviser":
-                    window.location.href= "http://127.0.0.1:5500/menu/underviser/underviserside.html"
+                    window.location.href= "http://127.0.0.1:5500/login/menu/underviser/underviserside.html"
                     document.cookie="loginPrivilege="+this.response+""
                     break;
 
                     case "":
-                    window.location.href= "http://127.0.0.1:5500/menu/underviser/underviserside.html"
+                    window.location.href= "http://127.0.0.1:5500/login/menu/underviser/underviserside.html"
                     document.cookie="loginPrivilege="+this.response+""
                 }
 
