dae1f5c35841afdddba9c92579d4b6b2920be078
kasp4770 <kasp4770@edu.easj.dk>
2018-11-07 16:26:38 +0100

    Søgefunktion og tilretning af frontend

--- Ordbog/public/javascripts/ordbog.js
+++ Ordbog/public/javascripts/ordbog.js
@@ -21,3 +21,18 @@ function slet_ord() {
     document.getElementById('slet_ord').style.display = 'block';
 }
 
+function search() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("searchInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("minListe");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
