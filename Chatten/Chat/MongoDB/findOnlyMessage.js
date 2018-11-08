25df4166e3797dc94929962d71448eb06e981897
Johan Stenboeg <joha6163@edu.easj.dk>
2018-11-08 14:25:10 +0100

    ChangesSince1425

--- Chatten/Chat/MongoDB/findOnlyMessage.js
+++ Chatten/Chat/MongoDB/findOnlyMessage.js
@@ -10,12 +10,12 @@ var dbNavn = "sprogAppChatDb";
 //Collection navn: c1
 var collectionNavn = "c1";
 //Antal beskeder der skal printes
-var collectionHentAntalBeskeder = 1;
+var collectionHentAntalBeskeder = 10000;
 
 MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
     if (err) throw err;
     var dbo = db.db(dbNavn);
-    dbo.collection(collectionNavn).find({ brugernavn:"JohanTest"}).project({ _id: 0, brugernavn: 0 }).sort({$natural:-1}).limit(collectionHentAntalBeskeder).toArray(function (err, result) {
+    dbo.collection(collectionNavn).find({ brugernavn:"Frank"}).project({ _id: 0, brugernavn: 0 }).sort({$natural:1}).limit(collectionHentAntalBeskeder).toArray(function (err, result) {
         if (err) throw err;
         console.log(result);
         db.close();
--- Chatten/database/insertPictureIDB.js
+++ Chatten/database/insertPictureIDB.js
@@ -1,4 +1,17 @@
-    //Opretter et MongoClient variabel og henter mongodb modulet
+/*function show_image(imgName, cN, alt){
+    var img = document.createElement("img");
+    img.src = src;
+    img.width = width;
+    img.height = height;
+    img.alt = alt;
+
+    document.body.appendChild(img)
+}   
+   
+   */
+   
+   
+   //Opretter et MongoClient variabel og henter mongodb modulet
     var MongoClient = require('mongodb').MongoClient;
 
     //SprogAppChatDB = sacdb
@@ -8,20 +21,28 @@
     //Database navn: sprogAppChatDb
     var dbNavn = "sprogAppChatDb";
     //Collection navn: c1
-    var collectionNavn = "c1";
-    //Antal beskeder der skal printes
+    var collectionNavn = "cb1";
 
-    //Connecting til databasen
-    MongoClient.connect(url, { useNewUrlParser: true }, function(err, db){
+    //Readline, tillader consol input til valg af collection
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+  //Spørger hvilken collection man vil søge i
+  readline.question(`What Collection do you want to insert picture reference into? `, (collectionNavn) => {
+       //Spørger hvilken collection man vil søge i
+   MongoClient.connect(url, { useNewUrlParser: true }, function(err, db){
+    if(err) throw err;
+    //Database navnet den skal connecte til
+    var dbo = db.db(dbNavn);
+    //Jsonfilen der skal insættes i databasen
+    var billedrefen = "https://www.dropbox.com/home/SprogApp2018?preview=waifu.jpg";
+    var jsonEnkelt = {billednavn:"test01", billedref:billedrefen}
+    //Vælger hvilken collection der skal indsættes data i og insætter data. 
+    dbo.collection(collectionNavn).insertOne(jsonEnkelt, function(err, db){
         if(err) throw err;
-        //Database navnet den skal connecte til
-        var dbo = db.db(dbNavn);
-        //Jsonfilen der skal insættes i databasen
-        var jsonEnkelt = {brugernavn:"JohanTest", besked:"Dette er en test"};
-
-        //Vælger hvilken collection der skal indsættes data i og insætter data. 
-        dbo.collection(collectionNavn).insertOne(jsonEnkelt, function(err, db){
-            if(err) throw err;
-        });
     });
-    
+    });
+
+});
+//Chatten\Billeder\waifu.jpg
