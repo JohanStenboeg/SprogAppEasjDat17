480db6169b2f6ac7c3da4b58434546d522d6dbce
Erik Ruhmanis <erik2310@edu.easj.dk>
2018-11-09 14:52:25 +0100

    Forbederelse til image upload til Google Cloud.

--- Ordbog/routes/ordbog.js
+++ Ordbog/routes/ordbog.js
@@ -3,39 +3,41 @@ var router = express.Router();
 var hbs = require('hbs');
 hbs.registerPartials(__dirname + '/views/partials');
 var mongoose = require('mongoose');
-var multer = require('multer');
+// var multer = require('multer');
 //var upload = multer({dest: "./public/uploads"});
-var Ord = require('../models/ordbogModel');
+// var Ord = require('../models/ordbogModel');
 mongoose.connect('mongodb://localhost:27017/tododb', { useNewUrlParser: true });
 var ordbogModel = require('../models/ordbogModel');
 var ordbog = mongoose.model('Ordbog', ordbogModel.ordbogSchema, 'ordbog');
 
-
-const storage = multer.diskStorage({
+/*
+var storage = multer.diskStorage({
   destination: function (req, file, callback) {
-    callback(null, './public/uploads/');
+    callback(null, './public/uploads');
   },
   filename: function (req, file, callback) {
-    callback(null, new Date().toISOString() + file.file.originalname);
+    callback(null, new Date() + '-' + file.originalname);
   }
 });
 
-const fileFilter = (req, file, callback) => {
+var fileFilter = (req, file, callback) => {
   // reject a file
   if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
     callback(null, true);
   } else {
     callback(null, false);
   }
-};
+}
 
-const upload = multer({
+
+var upload = multer({
   storage: storage,
   limits: {
-    filesize: 1024 * 1024 * 10
+    fileSize: 1024 * 1024 * 10
   },
   fileFilter: fileFilter
 });
+*/
 
 
 /* GET handler som henter ordbog siden med ordene */
@@ -98,7 +100,7 @@ router.post('/slet_ord', function (req, res, next) {
   });
 });
 
-
+/*
 router.post('/uploadimage2', upload.single('image'), function (req, res, next) {
   const ord = new Ord({
     _id: new mongoose.Types.ObjectId(),
@@ -128,14 +130,18 @@ router.post('/uploadimage2', upload.single('image'), function (req, res, next) {
       });
     });
 });
+*/
 
 
+/*
 router.post('/uploadimage', upload.single('image'), function (req, res) {
   if (req.file) {
     res.json(req.file);
   }
   else throw 'error';
 })
+*/
+
 
 
 module.exports = router;
