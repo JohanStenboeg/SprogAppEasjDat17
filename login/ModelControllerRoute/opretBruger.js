5e565c827a9d7649a6152002edbfde6a094f348f
Michael Hansen <michaelhansenit@live.dk>
2018-11-12 12:53:27 +0100

    opretBruger virker nu via localhost:3000/opretBruger

---
+++ login/ModelControllerRoute/opretBruger.js
@@ -0,0 +1,36 @@
+//Variabler til moduler der skal bruges
+var mongoose = require('mongoose');
+var express = require('express');
+var router = express.Router();
+
+//router fodbinder til css
+router.use('/css/style.css', function(req, res){
+    res.sendFile(__dirname + '/css/style.css');
+  });
+
+//Forbind til mongoDB vha. mongoose modulet.
+mongoose.connect('mongodb://localhost:27017/dbSprog', {useNewUrlParser: true});
+//Fortæller hvilket Model den skal hente og bruge
+var BrugerModel = require('../ModelControllerRoute/brugerModel');
+//Fortæller hvilken collection i MongoDB der skal bruges
+var bruger = mongoose.model('Bruger', BrugerModel.BrugerSchema, 'brugere');
+
+//Laver en POST forespørgsel på siden, og indsætter indhold i databasen fra sidens indhold
+router.post('/register', function(req, res, next){
+    //lokal variabel til at skabe et objekt, som databasen kan læse fra
+    let object = {
+        brugernavn : req.body.brugernavn,
+        kodeord : req.body.kodeord,
+        rolle : req.body.rolle,
+        rewardLvL : 0
+    }
+    //opret brugeren
+    bruger.create(object, function(err){
+        if(err){return console.log(err)}
+    });
+    //send mig til opret-siden
+    res.redirect('/opretBruger');
+});
+
+//udsend scriptet
+module.exports = router;
--- login/server.js
+++ login/server.js
@@ -10,18 +10,24 @@
 
 //programmet køres med >npm run start<
 var clienttoserver = require('./clienttoserver')
-var bodyParser     =   require("body-parser");
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 var mongoose = require('mongoose');
-  //Client = require('../ModelControllerRoute/clientModel'), //created model loading here
 var bodyParser = require('body-parser');
+var path = require('path');
   
-// mongoose instance connection url connection
-//mongoose.Promise = global.Promise;
-//mongoose.connect('mongodb://localhost/Tododb',{ useNewUrlParser: true }); 
+//Routes til scripts
+var opretRouter = require('./ModelControllerRoute/opretBruger');
 
+//Henter fra roden af URL og til indholdet af opretBruger.html
+app.get('/opretBruger', function(req, res){
+  res.sendFile(path.join(__dirname + '/menu/opretBruger.html'));
+});
+
+app.use('/css/style.css', function(req, res){
+  res.sendFile(__dirname + '/css/style.css');
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -40,7 +46,8 @@ app.use(function(req, res, next) {
 
 app.route('/login').post(clienttoserver.login_as_client)
 
- 
+//Fortæller hvilket route skal hentes indhold fra, og hente scripts fra
+app.use('/opretBruger', opretRouter);
 
 //Middleware : Skriver hvis den ikke kan finde siden.
 app.use(function(req, res) {
