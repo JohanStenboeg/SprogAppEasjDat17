1d0cc6dc207caba93953d5c6b4c5d07afd1697ac
Johan Stenboeg <joha6163@edu.easj.dk>
2018-11-12 13:12:53 +0100

    lots of modified node module packets

--- Chatten/chatServer.js
+++ Chatten/chatServer.js
@@ -49,8 +49,8 @@ app.get('/getChat', function(request, res){
 
 
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(8080, function(){
+  console.log('listening on *:8080');
 });
 
 io.on('connection', function(socket){
