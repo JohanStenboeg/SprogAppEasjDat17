947d8cf794ec6ec9098db6d77ee7402b22818041
Erik Ruhmanis <erik2310@edu.easj.dk>
2019-01-07 12:11:04 +0100

    Nu virker søge funktionen.

--- Ordbog/public/javascripts/ordbog.js
+++ Ordbog/public/javascripts/ordbog.js
@@ -1,3 +1,4 @@
+/*
 function search() {
     var input, filter, ul, li, a, i;
     input = document.getElementById("searchInput");
@@ -10,6 +11,23 @@ function search() {
             li[i].style.display = "";
         } else {
             li[i].style.display = "none";
+        }
+    }
+ }
+*/
+
+ function search2() {
+    var input, filter, card, card_body, card_title, i;
+    input = document.getElementById("searchInput");
+    filter = input.value.toUpperCase();
+    card = document.getElementsByClassName("card");
+    for (i = 0; i < card.length; i++) {
+        card_body = card[i].getElementsByClassName("card-body")[0];
+        card_title = card_body.getElementsByClassName("card-title")[0];
+        if (card_title.innerHTML.toUpperCase().search(filter) == 0) {
+            card[i].style.display = "";
+        } else {
+            card[i].style.display = "none";
         }
     }
  }
