35dabe7aa10eb65b1c28c8d76cbe4e619e4bdeeb
Erik Ruhmanis <erik2310@edu.easj.dk>
2019-01-09 14:31:01 +0100

    Det som Flenner sendte til mig virker nu.

--- Ordbog/routes/redigerord.js
+++ Ordbog/routes/redigerord.js
@@ -2,12 +2,12 @@ var express = require('express');
 var router = express.Router();
 var multer = require('multer');
 var MongoClient = require('mongodb').MongoClient;
-var ObjectId = require('mongodb').ObjectId;
+var ObjectId = require('mongodb').ObjectID;
 var url = "mongodb://localhost:27017/";
 
 router.get('/:id', function (req, res, next) {
 
-    MongoClient.connect(url, function (err, db) {
+    MongoClient.connect(url, { useNewUrlParser: true } ,function (err, db) {
         if (err) throw err;
         var dbo = db.db("tododb");
         dbo.collection("ordbog").findOne({ _id: ObjectId(req.params.id) }, function (err, result) {
@@ -22,10 +22,10 @@ router.get('/:id', function (req, res, next) {
 });
 
 router.post('/', function (req, res, next) {
-    MongoClient.connect(url, function (err, db) {
+    MongoClient.connect(url, { useNewUrlParser: true } , function (err, db) {
         if (err) throw err;
         var dbo = db.db("tododb");
-        dbo.collection("ordbog").updateOne({ _id: req.body.id }, { $set: { ord: req.body.ord, kategori: req.body.kategori }}, function (err, res) {
+        dbo.collection("ordbog").updateOne({ _id: ObjectId(req.body.id) }, { $set: { ord: req.body.ord, kategori: req.body.kategori }}, function (err, res) {
             if (err) throw err;
             console.log("1 document updated");
             db.close;
