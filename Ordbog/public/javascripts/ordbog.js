d89f0b44e1a5df27c232af92117bee2986514856
kasp4770 <kasp4770@edu.easj.dk>
2019-01-11 14:20:00 +0100

    Tilføjet: Til top knap

--- Ordbog/public/javascripts/ordbog.js
+++ Ordbog/public/javascripts/ordbog.js
@@ -42,3 +42,20 @@ function playAudio() {
     var audio = document.getElementById("audio");
     audio.play();
 }
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
